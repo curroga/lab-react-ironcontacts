f0c9f54fa0d531f6d7c5c1255e57fcb86d4108ce
Curro Garcia <curro-91@hotmail.com>
2022-11-01 20:57:32 +0100

    It1,2,3 done, falta darle estilos css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const initialList = contacts.slice(0, 5);
+  const [list, setList] = useState(initialList);
+
+  const addContacts = () => {
+    const randomIndex = Math.floor(Math.random() * contacts.length)
+    const contactAleatorio = contacts[randomIndex]
+    const copy = structuredClone(list)
+    copy.push(contactAleatorio)
+    setList(copy)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>IronContacts</h3>
+
+      <h5>Picture</h5>
+      <h5>Name</h5>
+      <h5>Popularity</h5>
+      <button onClick={addContacts}>Añadir Contacto</button>
+      <br />
+      {list.map((eachContact) => {
+        return (
+          <div key={eachContact}>
+            <img src={eachContact.pictureUrl} alt="picture" width="80px" />
+                
+            <p>{eachContact.name}</p>
+            <p>{eachContact.popularity.toFixed(2)}</p>
+            <p>{eachContact.wonOscar === true ? "🏆" : ""}</p>
+            <p>{eachContact.wonEmmy === true ? "🏆" : ""}</p>
+              
+            
+          </div>
+        );
+      })}
     </div>
   );
 }
